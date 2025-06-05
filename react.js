2f5c53caefcd086a49cd864dd702d6f698b01d2f
AshleyPairat2 <195990212+AshleyPairat2@users.noreply.github.com>
2025-06-05 10:49:42 +0800

    added profile picture

---
+++ react.js
@@ -0,0 +1,8 @@
+// Placeholder for future interactivity
+document.querySelector(".subscribe-btn").addEventListener("click", () => {
+  alert("Thanks for subscribing!");
+});
+
+document.querySelector(".message-btn").addEventListener("click", () => {
+  alert("Message sent!");
+});
